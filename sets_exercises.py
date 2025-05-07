01d26f0daa36061b613b05a5a2fe328a251273ff
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-05-07 11:05:11 -0300

    Update sets_exercises.py

--- sets_exercises.py
+++ sets_exercises.py
@@ -12,3 +12,16 @@ def remove_duplicates(my_list):
     my_set = set(my_list)
     new_list = list(my_set)
     return new_list
+
+def longest_consecutive_sequence(nums):
+    num_set = set(nums)
+    longest_sequence = 0 
+    for num in nums:
+        if num - 1 not in num_set:
+            current_num = num 
+            current_sequence = 1 
+            while current_num + 1 in num_set:
+                current_num += 1 
+                current_sequence += 1 
+            longest_sequence = max(longest_sequence, current_sequence)
+    return longest_sequence
