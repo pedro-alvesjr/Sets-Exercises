62116a503336c82c46ed0fa5990ae308ca652baf
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-05-08 10:53:34 -0300

    Update sets_exercises.py
    Added tests to each method.

--- sets_exercises.py
+++ sets_exercises.py
@@ -7,7 +7,6 @@ def find_pairs(arr1, arr2, target):
             pairs.append((complement, num))
     return pairs
 
-
 def remove_duplicates(my_list):
     my_set = set(my_list)
     new_list = list(my_set)
@@ -25,3 +24,11 @@ def longest_consecutive_sequence(nums):
                 current_sequence += 1 
             longest_sequence = max(longest_sequence, current_sequence)
     return longest_sequence
+
+# Testing each method:
+
+print(find_pairs((3, 2, 5, 6), (4, 7, 9, 0, 3), 10))
+
+print(remove_duplicates((2, 3, 4, 3, 4, 6, 10)))
+
+print(longest_consecutive_sequence((2, 3, 4, 6, 7, 8, 9, 13, 14)))
